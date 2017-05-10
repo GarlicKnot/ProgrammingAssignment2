e4c9d7126ac0288967deb23021f809ac912027c5
Will Sennett <will.sennett@gmail.com>
2017-05-10 13:32:04 -0400

    Peer reviewed doc commit

---
+++ Assignment2.R
@@ -0,0 +1,23 @@
+factorycachefunction <- function(x) {
+  m = NULL
+  set    <- function(y) {
+    x   <<- y
+    m   <<- NULL
+  }
+  get    <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
+}
+
+inversefunction <- function(x,...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinv(m)
+  m
+}
