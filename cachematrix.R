1ddfc9ce5cf7af38030b75f8d8f15df896f96ff7
Will Sennett <will.sennett@gmail.com>
2017-05-10 13:37:39 -0400

    Updated function and file names to match names in the class

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Inputs a square matrix into a cache function with list output
+## Intended for inversion in cacheSolve
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())  {
+  m = NULL
+  set    <- function(y) {
+    x   <<- y
+    m   <<- NULL
+  }
+  get    <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Takes a matrix converted to list through 'make CacheMatrix'
+## Checks for cached data, and returns inverse matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinv(m)
+  m
 }
